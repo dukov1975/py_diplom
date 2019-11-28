73410362a3bfcd46c00ca41c96d609026b2d60a1
dukov1975 <dukov@icloudpocket.ru>
2019-11-28 10:51:41 +0300

    py diplom

--- py_diplom/settings.py
+++ py_diplom/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend.apps.BackendConfig',
+    'backend.apps.ApiConfig',
     'rest_framework',
     'rest_framework.authtoken',
     'django_rest_passwordreset',
